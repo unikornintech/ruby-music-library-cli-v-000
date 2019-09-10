75125e382fc3c9595730a0944b164e842e2913b4
Lauren Days <lmchambers0420@gmail.com>
2019-09-10 11:32:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -49,5 +49,7 @@ class MusicLibraryController
     if artist = Artist.find_by_name(input)
       artist.songs.sort_by(&:name).each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
   end 
 end 
