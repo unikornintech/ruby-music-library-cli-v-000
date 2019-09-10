806e38db638d3d13c19b04aba7cbe517e1ae1ee0
Lauren Days <lmchambers0420@gmail.com>
2019-09-10 11:18:32 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,7 +36,7 @@ class MusicLibraryController
   
   def list_genres
     Genre.all.sort_by(&:name).each.with_index(1) do |genre, index|
-      puts "#{index}, #{genre.name}"
+      puts "#{index}. #{genre.name}"
     end 
   end
 end 
